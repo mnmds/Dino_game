887e1c81511b3dc81d285dff3256dcecb4d58651
MSC <95544952+mISHAKostikov@users.noreply.github.com>
2024-08-24 00:10:34 +0300

    0.0.31

--- Packages/Frontend/Components/Subscribe/Subscribe.js
+++ Packages/Frontend/Components/Subscribe/Subscribe.js
@@ -1,4 +1,5 @@
 import {Components} from '../../../Global/Frontend/Frontend.js';
+import {ShopButton} from '../ShopButton/ShopButton.js';
 
 
 export class Subscribe extends Components.Component {
@@ -8,15 +9,57 @@ export class Subscribe extends Components.Component {
 
     static _attributes = {
         ...super._attributes,
+        task: '',
+        image: '',
+        value: 0,
+        status: 'sale',
+        link: '',
     };
 
-    static _elements = {};
+    static _elements = {
+        task: '',
+        image: '',
+        reward: '',
+    };
 
     static _eventListeners = {};
 
 
     static {
         this.init();
+    }
+
+    get task() {
+        return this._attributes.task;
+    }
+    set task(task) {
+        this._attribute__set('task', task);
+        this._elements.task.textContent = task;
+    }
+
+    get image() {
+        return this._attributes.image;
+    }
+    set image(image) {
+        this._attribute__set('image', image);
+        this._elements.image.src = `./Packages/Frontend/Components/Subscribe/${image}.png`;
+    }
+
+    get value() {
+        return this._attributes.value;
+    }
+    set value(value) {
+        this._attribute__set('value', value);
+        this._elements.reward.statuses_values.sale.text = value;
+        this._elements.reward.status_value__refresh();
+    }
+
+    get status() {
+        return this._attributes.status;
+    }
+    set status(status) {
+        this._attribute__set('status', status);
+        this._elements.reward.status = status;
     }
 
 
