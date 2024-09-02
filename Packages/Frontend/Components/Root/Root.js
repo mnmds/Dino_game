5bb0f5365c5ac6e3c250c1723df631e9c87f4ac4
NerkChere5 <87468612+NerkChere5@users.noreply.github.com>
2024-09-02 14:02:02 +0300

    0.0.105
    test

--- Packages/Frontend/Components/Root/Root.js
+++ Packages/Frontend/Components/Root/Root.js
@@ -84,7 +84,8 @@ export class Root extends Components.Viewport {
 
 
     async _init() {
-        await this.user_get();
+        alert(import.meta.url)
+        // await this.user_get();
     }
 
     _on__back_click() {
--- Packages/Frontend/Main/Main.js
+++ Packages/Frontend/Main/Main.js
@@ -1,20 +1,16 @@
 import {Root} from '../Components/Root/Root.js';
-// import {Components} from '../../Global/Frontend/Frontend.js';
+import {Components} from '../../Global/Frontend/Frontend.js';
 // import {Units} from '../../Global/Frontend/Frontend.js';
 
 
 alert(import.meta.url)
-// Components.Component.init({
-//     styleSheets_descriptors: {
-//         dark: new URL('../Theme/Theme.css', import.meta.url),
-//     },
-// });
-// // import {Telegram} from '../Api/Units/Telegram/Telegram.js';
-// // import 'https://telegram.org/js/telegram-web-app.js';
-
-
-// window.addEventListener('DOMContentLoaded', () => {
-//     Units.Telegram.app__init();
-// //     // window.Telegram.WebApp.expand();
-// //     // window.Telegram.WebApp.ready();
-// });
+Components.Component.init({
+    styleSheets_descriptors: {
+        dark: new URL('../Theme/Theme.css', import.meta.url),
+    },
+});
+window.addEventListener('DOMContentLoaded', () => {
+    Units.Telegram.app__init();
+//     // window.Telegram.WebApp.expand();
+//     // window.Telegram.WebApp.ready();
+});
