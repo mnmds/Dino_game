bbc2876fc4a634ea67365f399bcf543b009e88a2
NerkChere5 <87468612+NerkChere5@users.noreply.github.com>
2024-09-02 13:53:51 +0300

    0.0.105
    test

--- Packages/Frontend/Main/Main.js
+++ Packages/Frontend/Main/Main.js
@@ -1,14 +1,14 @@
 import {Root} from '../Components/Root/Root.js';
-import {Components} from '../../Global/Frontend/Frontend.js';
-import {Units} from '../../Global/Frontend/Frontend.js';
+// import {Components} from '../../Global/Frontend/Frontend.js';
+// import {Units} from '../../Global/Frontend/Frontend.js';
 
 
-alert(1)
-Components.Component.init({
-    styleSheets_descriptors: {
-        dark: new URL('../Theme/Theme.css', import.meta.url),
-    },
-});
+alert(import.meta.url)
+// Components.Component.init({
+//     styleSheets_descriptors: {
+//         dark: new URL('../Theme/Theme.css', import.meta.url),
+//     },
+// });
 // // import {Telegram} from '../Api/Units/Telegram/Telegram.js';
 // // import 'https://telegram.org/js/telegram-web-app.js';
 
