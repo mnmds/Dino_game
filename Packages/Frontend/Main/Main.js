6a0c11f9b92798e6879e770d0117e3bebed86b01
NerkChere5 <87468612+NerkChere5@users.noreply.github.com>
2024-09-02 13:44:02 +0300

    0.0.105
    test

--- Packages/Frontend/Main/Main.js
+++ Packages/Frontend/Main/Main.js
@@ -2,6 +2,8 @@ import {Root} from '../Components/Root/Root.js';
 import {Components} from '../../Global/Frontend/Frontend.js';
 import {Units} from '../../Global/Frontend/Frontend.js';
 
+
+alert(1)
 Components.Component.init({
     styleSheets_descriptors: {
         dark: new URL('../Theme/Theme.css', import.meta.url),
