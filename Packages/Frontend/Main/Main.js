00bf31fc6321db0df416623db61c22717ccc68ba
NerkChere5 <87468612+NerkChere5@users.noreply.github.com>
2024-09-02 14:29:51 +0300

    0.0.105
    test

--- Packages/Frontend/Main/Main.js
+++ Packages/Frontend/Main/Main.js
@@ -5,13 +5,13 @@ import {Units} from '../../Global/Frontend/Frontend.js';
 
 
 alert(`${import.meta.url}, ${Settings}`)
-Components.Component.init({
-    styleSheets_descriptors: {
-        dark: new URL('../Theme/Theme.css', import.meta.url),
-    },
-});
-window.addEventListener('DOMContentLoaded', () => {
-    Units.Telegram.app__init();
+// Components.Component.init({
+//     styleSheets_descriptors: {
+//         dark: new URL('../Theme/Theme.css', import.meta.url),
+//     },
+// });
+// window.addEventListener('DOMContentLoaded', () => {
+//     Units.Telegram.app__init();
 //     // window.Telegram.WebApp.expand();
 //     // window.Telegram.WebApp.ready();
 });
