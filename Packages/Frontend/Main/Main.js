da03a603a9b366b83950779912421e2a2910617e
NerkChere5 <87468612+NerkChere5@users.noreply.github.com>
2024-09-08 11:35:10 +0300

    0.1.6
    test

--- Packages/Frontend/Main/Main.js
+++ Packages/Frontend/Main/Main.js
@@ -1,20 +1,17 @@
 import {Root} from '../Components/Root/Root.js';
 import {Components} from '../../Global/Frontend/Frontend.js';
+import {Units} from '../../Global/Frontend/Frontend.js';
 
 Components.Component.init({
     styleSheets_descriptors: {
         dark: new URL('../Theme/Theme.css', import.meta.url),
     },
 });
-window.addEventListener('error', (event) => {
-    alert(event.filename + ' ' + event.message + ' ' + event.lineno);
-});
-// import {Telegram} from '../Api/Units/Telegram/Telegram.js';
-// import 'https://telegram.org/js/telegram-web-app.js';
 
-
-// window.addEventListener('DOMContentLoaded', () => {
-//     Telegram.app__init();
-//     // window.Telegram.WebApp.expand();
-//     // window.Telegram.WebApp.ready();
+// window.addEventListener('error', (event) => {
+//     alert(event.filename + ' ' + event.message + ' ' + event.lineno);
 // });
+
+window.addEventListener('DOMContentLoaded', () => {
+    Units.Telegram.app__init();
+});
